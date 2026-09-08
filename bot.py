611ea510c9838f4c0f969bf4ac6baf979f5ed58a
Jakub Szuchalski <200734576+KubaZx@users.noreply.github.com>
2026-09-08 02:40:23 +0200

    Move profile command to Profile cog

--- bot.py
+++ bot.py
@@ -78,16 +78,6 @@ async def on_message(message):
         await message.channel.send("Achievement unlocked! You reached 500 messages!")
         save_data(players)
 
-# player profile, showing level ; xp ; money
-    elif message.content.lower() == '!profile':
-        player_data = players[person_key]
-        embed = discord.Embed(title='Player Profile', color=0x00ff00)
-        embed.add_field(name='Level', value=player_data['level'], inline=True)
-        embed.add_field(name='XP', value=f"{player_data['xp']}/{player_data['level'] * 100}", inline=True)
-        embed.add_field(name='Money', value=player_data['money'], inline=True)
-        embed.set_thumbnail(url=message.author.display_avatar.url)
-        await message.channel.send(embed=embed)
-
 # Profile of achievements
     elif message.content.lower() == '!achievements':
         achievements_data = players[person_key]
--- cogs/profile.py
+++ cogs/profile.py
@@ -11,6 +11,17 @@ class Profile(commands.Cog):
     async def card(self, ctx):
         await ctx.send("Work in progress 🫡")
 
+    #Player profile, showing level; xp ; money
+    @commands.command(name='profile')
+    async def profile(self, ctx):
+        person_key = f"{ctx.author.id}_{ctx.guild.id}"
+        player_data = players[person_key]
+        embed = discord.Embed(title='Player Profile', color=0x00ff00)
+        embed.add_field(name='Level', value=player_data['level'], inline=True)
+        embed.add_field(name='XP', value=f"{player_data['xp']}/{player_data['level'] * 100}", inline=True)
+        embed.add_field(name='Money', value=player_data['money'], inline=True)
+        embed.set_thumbnail(url=ctx.author.display_avatar.url)
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Profile(bot))

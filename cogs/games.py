9ff36d358d14ae5ad35730337ccd39bfba40ff97
Jakub Szuchalski <200734576+KubaZx@users.noreply.github.com>
2026-09-12 06:19:12 +0200

    Convert Games cog to slash commands

--- cogs/games.py
+++ cogs/games.py
@@ -2,6 +2,7 @@ import discord
 import random
 from discord.ext import commands
 from discord import app_commands
+from typing import Literal
 
 class Games(commands.Cog):
     def __init__(self, bot):
@@ -16,31 +17,28 @@ class Games(commands.Cog):
         await interaction.response.send_message(f"You rolled {random.randint(1, sides)}!")
 
     # Coinflip - 50/50
-    @commands.command(name='coinflip')
-    async def coinflip(self, ctx):
+    @app_commands.command(name='coinflip', description='Flip a coin')
+    async def coinflip(self, interaction: discord.Interaction):
         coin = ['heads', 'tails']
-        await ctx.send(random.choice(coin))
+        await interaction.response.send_message(random.choice(coin))
 
     # Choose between yes/maybe/no
-    @commands.command(name='choose')
-    async def choose(self, ctx):
+    @app_commands.command(name='choose', description='Let the bot decide for you')
+    async def choose(self, interaction: discord.Interaction):
         answers = ['yes', 'maybe', 'no']
-        await ctx.send(random.choice(answers))
+        await interaction.response.send_message(random.choice(answers))
 
     # Game of rock, paper and scissors
-    @commands.command(name='rps', usage='paper')
-    async def rps(self, ctx, player_choice: str = ""):
+    @app_commands.command(name='rps', description='Play rock-paper-scissors')
+    async def rps(self, interaction: discord.Interaction, player_choice: Literal['rock', 'paper', 'scissors']):
         choices = ['rock', 'paper', 'scissors']
-        if player_choice not in choices:
-            await ctx.send("Wrong choice! Pick rock, paper or scissors")
-            return
         bot_choice = random.choice(choices)
         if player_choice == bot_choice:
-            await ctx.send(f"You picked {player_choice} and bot picked {bot_choice}, Draw!")
+            await interaction.response.send_message(f"You picked {player_choice} and bot picked {bot_choice}, Draw!")
         elif (player_choice == 'rock' and bot_choice == 'scissors') or (player_choice == 'paper' and bot_choice == 'rock') or (player_choice == 'scissors' and bot_choice == 'paper'):
-            await ctx.send(f"You won! Bot picked {bot_choice}")
+            await interaction.response.send_message(f"You won! Bot picked {bot_choice}")
         else:
-            await ctx.send(f"Bot picked {bot_choice}, You lost!")
+            await interaction.response.send_message(f"Bot picked {bot_choice}, You lost!")
 
 async def setup(bot):
     await bot.add_cog(Games(bot))
